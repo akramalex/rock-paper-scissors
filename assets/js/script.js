b652c2ee2decaf4618524732f2b0506d3b77762f
akramalex <>
2024-02-01 15:44:35 +0000

    (js) fixed warring for evenlistener loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,11 +14,14 @@ const choices = ["rock", "paper", "scissors"];
 /**
  * add event listener to all the buttons 
  */
+
+function handleButtonClick() {
+    let playerChoice = this.getAttribute("data-choice");
+    playGame(playerChoice);
+}
+
 for (let button of buttons) {
-    button.addEventListener("click", function () {
-        let playerChoice = this.getAttribute("data-choice");
-        playGame(playerChoice);
-    });
+    button.addEventListener("click", handleButtonClick);
 }
 
 /**
@@ -34,7 +37,7 @@ function playGame(playerChoice) {
     computerImage.src = `./assets/images/${choices[computerChoice]}.png`;
     computerImage.alt = choices[computerChoice];
     
- let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+     let result = checkWinner(choices[computerChoice], choices[playerChoice]);
     updateScore(result);
 }
 
